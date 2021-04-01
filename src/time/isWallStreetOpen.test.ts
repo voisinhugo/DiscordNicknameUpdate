a1731669ea02b13fde6057dd7ce7310550b07b37
Hugo Voisin <hugov@bam.tech>
2021-04-01 19:14:33 +0200

    test: add basic test for time handling

---
+++ src/time/isWallStreetOpen.test.ts
@@ -0,0 +1,16 @@
+import { DateTime } from 'luxon';
+import { isWeekday } from './isWallStreetOpen';
+
+describe('isWeekday', () => {
+  it('returns true when a week day is given', () => {
+    const dt = DateTime.fromISO("2021-03-26T15:42:23.123"); // Friday
+    expect(isWeekday(dt)).toEqual(true);
+  })
+
+  it('returns false when a weekend day is given', () => {
+    const dt = DateTime.fromISO("2021-03-27T15:42:23.123"); // Saturday
+    const dt2 = DateTime.fromISO("2021-03-28T15:42:23.123"); // Sunday
+    expect(isWeekday(dt)).toEqual(false);
+    expect(isWeekday(dt2)).toEqual(false);
+  })
+});
