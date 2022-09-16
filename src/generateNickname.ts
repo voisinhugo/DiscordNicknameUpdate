9ba022a80f7277726f8f3c25ad997e773fa6f745
Hugo Voisin <hugov@bam.tech>
2022-09-16 23:45:14 +0200

    feat: take into account the GME share split

--- src/generateNickname.ts
+++ src/generateNickname.ts
@@ -1,11 +1,16 @@
 const GME_VALUE_WHEN_BOUGHT = 84.68;
 
 export const generateNickname = (GMEValue: number) => {
-  if (GMEValue > GME_VALUE_WHEN_BOUGHT) {
-    const percent = (GMEValue / GME_VALUE_WHEN_BOUGHT - 1) * 100;
+  // There was a stock split on July 22 on a 4-for-1 basis
+  const GMEValueBeforeShareSplit = GMEValue * 4;
+
+  if (GMEValueBeforeShareSplit > GME_VALUE_WHEN_BOUGHT) {
+    const percent =
+      (GMEValueBeforeShareSplit / GME_VALUE_WHEN_BOUGHT - 1) * 100;
     return `Hugameriche 📈 +${percent.toFixed(2)}%  📈`;
   } else {
-    const percent = (1 - GMEValue / GME_VALUE_WHEN_BOUGHT) * 100;
+    const percent =
+      (1 - GMEValueBeforeShareSplit / GME_VALUE_WHEN_BOUGHT) * 100;
     return `Hugamepauvre 📉 -${percent.toFixed(2)}%  📉`;
   }
 };
