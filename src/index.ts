a3712567d33fc933ab2bb0a9f207c16c14530596
Hugo Voisin <hugov@bam.tech>
2022-09-17 13:11:47 +0200

    Revert "feat: use cron config to run the script each day"
    This reverts commit bdd36b1ae14cff4127981324cbe80530636e3168.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
-import cron from "node-cron";
-
 import { getStockValue } from "./stock_market/getStockValue";
 import { changeNickname, changeBotNickname } from "./discord/changeNickname";
 import { isWallStreetOpen } from "./time/isWallStreetOpen";
+import { sleep } from "./utils/sleep";
 import { generateNickname, generateBotNickname } from "./generateNickname";
 
+const LOOP_PERIOD = 900_000; // 15min
+
 const main = async () => {
-  if (isWallStreetOpen()) {
-    const GMEValue = await getStockValue("GME");
+  let count = 0;
 
-    const nickname = generateNickname(GMEValue);
-    await changeNickname(nickname);
+  while (count < 900) {
+    if (isWallStreetOpen()) {
+      const GMEValue = await getStockValue("GME");
 
-    const botNickname = generateBotNickname(GMEValue);
-    await changeBotNickname(botNickname);
-  } else {
-    console.log("Wall Street is closed...");
+      const nickname = generateNickname(GMEValue);
+      await changeNickname(nickname);
+
+      const botNickname = generateBotNickname(GMEValue);
+      await changeBotNickname(botNickname);
+
+      count++;
+    } else {
+      console.log("Wall Street is closed...");
+    }
+    await sleep(LOOP_PERIOD);
   }
 };
 
-cron.schedule("0 16 * * *", main);
+main();
