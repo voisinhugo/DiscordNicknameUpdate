669e50dbedc8b48567d2614cf0db4bb7a94714c3
Hugo Voisin <hugov@bam.tech>
2022-09-17 13:13:13 +0200

    feat: stop looping as the script will be used in a cron

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 import { getStockValue } from "./stock_market/getStockValue";
 import { changeNickname, changeBotNickname } from "./discord/changeNickname";
 import { isWallStreetOpen } from "./time/isWallStreetOpen";
-import { sleep } from "./utils/sleep";
 import { generateNickname, generateBotNickname } from "./generateNickname";
 
-const LOOP_PERIOD = 900_000; // 15min
-
 const main = async () => {
-  let count = 0;
+  if (isWallStreetOpen()) {
+    const GMEValue = await getStockValue("GME");
 
-  while (count < 900) {
-    if (isWallStreetOpen()) {
-      const GMEValue = await getStockValue("GME");
+    const nickname = generateNickname(GMEValue);
+    await changeNickname(nickname);
 
-      const nickname = generateNickname(GMEValue);
-      await changeNickname(nickname);
-
-      const botNickname = generateBotNickname(GMEValue);
-      await changeBotNickname(botNickname);
-
-      count++;
-    } else {
-      console.log("Wall Street is closed...");
-    }
-    await sleep(LOOP_PERIOD);
+    const botNickname = generateBotNickname(GMEValue);
+    await changeBotNickname(botNickname);
+  } else {
+    console.log("Wall Street is closed...");
   }
 };
 
